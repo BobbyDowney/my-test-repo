5ab538b622d763b17eddfa93134958fcb90091fb
Jacob Cremers <jcremers@college.harvard.edu>
2021-05-25 08:48:17 -0400

    Had app go first to home page instead of sign in

--- contra/lib/main.dart
+++ contra/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/signin',
+      initialRoute: '/homepage',
         onGenerateRoute: RouteGenerator.generateRoute
     );
   }
