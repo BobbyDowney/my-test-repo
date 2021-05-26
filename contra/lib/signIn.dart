175bf2634b51cc5a77c75b07bfc1aeab01d251e4
Jacob Cremers <jcremers@college.harvard.edu>
2021-05-25 20:09:50 -0400

    Added very basic front-end for login, with button taking you to home page

--- contra/lib/signIn.dart
+++ contra/lib/signIn.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'navigationBar.dart';
+import 'route_generator.dart';
 
 class SignInScreen extends StatefulWidget {
   SignInScreen({Key? key}) : super(key: key);
@@ -12,12 +13,63 @@ class _SignInScreenState extends State<SignInScreen>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      body: LoginPage()
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+class _LoginPageState extends State<LoginPage>{
+  late String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Sign in page')]
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                validator: (input) {
+                  if ((input == null)| (input!.isEmpty)) {
+                    return 'Please type your Email';
+                  }
+                },
+                onSaved: (input) => _email = input ?? '',
+                decoration: InputDecoration(
+                  labelText: 'Email'
+                )
+              ),
+              TextFormField(
+                  validator: (input) {
+                    if ((input == null)| (input!.isEmpty)) {
+                      return 'Please type your Password';
+                    }
+                  },
+                  onSaved: (input) => _email = input ?? '',
+                  decoration: InputDecoration(
+                      labelText: 'Password'
+                  ),
+                obscureText: true
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.of(this.context).pushReplacementNamed('/homepage');
+                },
+                child: Text('Sign in')
+              )
+            ]
+          ),
         )
-      ), bottomNavigationBar: NavigationBar(0)
+      )
     );
   }
 }
